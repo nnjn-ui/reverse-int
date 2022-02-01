fc898fb018499a44127221fb1dd6558630256e16
nnjn <danil.bygay@gmail.com>
2022-02-02 00:51:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = function reverse () {
-  it('Should return 8965 when 5698 given', () => {
-    const reversed = reverse(5698);
-    return(reversed)
-    assert.equal(reversed, 8965);
-  });
+    let num = 5678
+  function reverse(num) {
+      
+    }
 }

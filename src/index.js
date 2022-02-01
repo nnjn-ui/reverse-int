da31f79c10b3b6ba9c4961f4e3acad13d754b24b
nnjn <danil.bygay@gmail.com>
2022-02-01 10:14:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 module.exports = function reverse (n) {
-  let res = n.toString().split('');
-  if (res[0] === '-') {
-      res.splice(0,1);
-  }
-  return +res.reverse().join('');
+  const reversed = reverse(789);
+  console.log(reverse);
 }
 
-const result = reverse(341);
-console.log(result);
-console.log(typeof(re));
+

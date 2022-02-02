785fc74d2e0eb24de6283cd90dd7fe82f3dbe629
nnjn <danil.bygay@gmail.com>
2022-02-03 00:00:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 module.exports = function reverse () {
 
-  let num = 4562;
-    
-    function reversDigits(num) {
-        let rev_num = 0;
-        while(num > 0)
-        {
-            rev_num = rev_num * 10 + num % 10;
-            num = Math.floor(num / 10);
-        }
-        return rev_num;
-    }
-
 }
 
+let num = 4562;
+    
+function reversDigits(num) {
+    let rev_num = 0;
+    while(num > 0)
+    {
+        rev_num = rev_num * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+    return rev_num;
+}
 
     

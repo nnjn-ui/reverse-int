906256d9cd2a86feda06764cd4cf728b5ceb3714
nnjn <danil.bygay@gmail.com>
2022-02-05 13:25:52 +0100

    fix: upload file

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ module.exports = function reverse (n) {
             if (n < 0) {
                 var temp = '';
             } else {
-                temp = stringfromNumber[i]; 
+                
                 stringfromNumber[i] = stringfromNumber[stringfromNumber.length - 1]
-                stringfromNumber[stringfromNumber.length - 1 - i] = temp; 
+                stringfromNumber[stringfromNumber.length - 1]; 
             }
             var temp = '';//1 
             temp = stringfromNumber[i]; 
